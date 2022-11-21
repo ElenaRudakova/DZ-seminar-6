86cc72359472e968b22491dcaee6c329fb038fe5
ElenaRudakova <er171173@gmail.com>
2022-11-21 21:48:18 +0300

    Добавить задачу 25

---
+++ Zadacha 41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int CoutnPositiv(int[] Array)
+{
+    int sum = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+        {
+            sum = sum + 1;
+        }
+    }
+    return sum;
+}
+Console.Clear();
+Console.WriteLine("Введите три числа через пробел: ");
+string input = Console.ReadLine()!;
+int[] Array = GetArrayFromString(input);
+int countNum = CoutnPositiv(Array);
+Console.WriteLine($"Количество положительных чисел равно: {countNum}");
